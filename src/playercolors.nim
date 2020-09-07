7ee8f8587eff4c2646f56987d95db1e5b5e3c539
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2020-09-06 20:57:49 -0700

    Add `$`(MCPlayerColor)

--- src/playercolors.nim
+++ src/playercolors.nim
@@ -2,6 +2,9 @@ type
   MCPlayerColor* = enum
     mccWhite, mccBlack
 
+proc `$`*(c: MCPlayerColor): string =
+  if c == mccWhite: "white" else: "black"
+
 proc oppositeColor*(c: MCPlayerColor): MCPlayerColor =
   if c == mccWhite: return mccBlack
   else: return mccWhite
