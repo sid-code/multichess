5016cf774e66c8e2c838e154955259252707bf69
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2020-09-07 18:49:36 -0700

    Remove main test program

--- src/multichess.nim
+++ src/multichess.nim
@@ -1,14 +1,1 @@
 import boards, games, pieces, playercolors, positions, moves, latticenodes, layouts, startpos, moverules
-
-# simple test program
-when isMainModule:
-  let f = 5
-  let r = 5
-  let squares = newSeq[MCSquare](f * r)
-  var sp = MCBoard(numFiles: 5, numRanks: 5, squares: squares)
-
-  sp[0, 0] = (mcpKnight, mccWhite)
-
-  let g = newGame(sp)
-  let kp = pos(g.rootNode, 0, 0)
-  echo g.rootNode.board
