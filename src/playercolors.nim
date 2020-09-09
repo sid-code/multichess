9e57aaaf193dbf71f4d3ec456c1315c78380d6c5
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2020-09-09 14:42:33 -0700

    Remove `$` from playercolor
    this was screwing things up

--- src/playercolors.nim
+++ src/playercolors.nim
@@ -2,9 +2,6 @@ type
   MCPlayerColor* = enum
     mccWhite, mccBlack
 
-proc `$`*(c: MCPlayerColor): string =
-  if c == mccWhite: "white" else: "black"
-
 proc oppositeColor*(c: MCPlayerColor): MCPlayerColor =
   if c == mccWhite: return mccBlack
   else: return mccWhite
