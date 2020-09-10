47d3434649579931d81b94abbc49409ceed31367
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2020-09-10 12:18:15 -0700

    Improve peerjs bindings
    Mainly add configuration in the constructor

--- src/peerjs.nim
+++ src/peerjs.nim
@@ -5,6 +5,7 @@ type
   PeerId* = cstring
   Peer* = ref PeerObj
   PeerObj {.importc.} = object of RootObj
+    id*: PeerId
   DataConnection* = ref DataConnectionObj
   DataConnectionObj {.importc.} = object of RootObj
     peer*: cstring
@@ -12,7 +13,22 @@ type
     message*: cstring
     `type`*: cstring
 
-proc newPeer*(): Peer {.importcpp: "new Peer()", nodecl.}
+proc newPeer*(id: cstring = nil,
+              host = cstring"0.peerjs.com",
+              path = cstring"/",
+              port = 443,
+              pingInterval = 5000,
+              secure = true,
+              debug = 0): Peer {.importcpp: """new Peer(#, {
+  host: #,
+  path: #,
+  port: #,
+  pingInterval: #,
+  secure: #,
+  debug: #,
+})
+""".}
+
 proc on*[T](p: Peer, event: cstring, fn: proc(v: T)) {.importcpp: "#.on(#, #)", nodecl.}
 proc connect*(p: Peer, id: cstring): DataConnection {.importcpp: "#.connect(#)", nodecl.}
 proc on*[T](p: DataConnection, event: cstring, fn: proc(v: T)) {.importcpp: "#.on(#, #)", nodecl.}
